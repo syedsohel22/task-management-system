406b0f3d0188210e3b837567094ec6dfbae78b29
sohel <mr.sohelsyed@gmail.com>
2023-07-09 01:04:02 +0530

    login done

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { UserModel } = require("../models/user.model");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const userRouter = express.Router();
 
 userRouter.post("/register", async (req, res) => {
@@ -30,8 +31,26 @@ userRouter.post("/register", async (req, res) => {
   }
 });
 
-userRouter.post("/login", (req, res) => {
+userRouter.post("/login", async (req, res) => {
   //login
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+          let token = jwt.sign({ userID: user._id, user: user.name }, "settyl");
+          res.status(200).json({ mag: "Logged In Successful.", token });
+        } else {
+          res.json({ error: "wrong email or passsword" });
+        }
+      });
+    } else {
+      res.json({ msg: "user does not exits..!" });
+    }
+  } catch (error) {
+    res.json({ error: error.message });
+  }
 });
 
 module.exports = {
